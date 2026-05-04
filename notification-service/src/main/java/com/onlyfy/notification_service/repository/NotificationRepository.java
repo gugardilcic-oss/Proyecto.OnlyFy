24a73524a5041f10f90a9db54ac979a14f276dec
gugardilcic-oss <gu.gardilcic@duocuc.cl>
2026-05-04 17:04:29 -0400

    Delete notification-service directory

--- notification-service/src/main/java/com/onlyfy/notification_service/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlyfy.notification_service.repository;
-
-import com.onlyfy.notification_service.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Integer> {
-}
