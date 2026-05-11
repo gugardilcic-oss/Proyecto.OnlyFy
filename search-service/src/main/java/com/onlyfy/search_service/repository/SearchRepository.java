77fb570614e20f253660da45914085c4e4fe25d6
Lukazez <lukazez17@gmail.com>
2026-05-11 17:21:33 -0400

    Delete search-service directory

--- search-service/src/main/java/com/onlyfy/search_service/repository/SearchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlyfy.search_service.repository;
-
-import com.onlyfy.search_service.model.SearchRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SearchRepository extends JpaRepository<SearchRecord, Integer> {
-}
