7e9982774dde7231967aa3e874cb686c8b911449
Lukazez <lukazez17@gmail.com>
2026-05-11 17:21:14 -0400

    Delete music-category-service directory

--- music-category-service/src/main/java/com/onlyfy/music_category_service/repository/MusicCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlyfy.music_category_service.repository;
-
-import com.onlyfy.music_category_service.model.MusicCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MusicCategoryRepository extends JpaRepository<MusicCategory, Integer> {
-}
