498f6281148d5ba6099852f7ae0111a3b7d2f223
Lukazez <lukazez17@gmail.com>
2026-05-11 17:21:24 -0400

    Delete recommendation-service/recommendation-service directory

--- recommendation-service/recommendation-service/src/main/java/com/onlyfy/recommendation_service/repository/RecommendationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlyfy.recommendation_service.repository;
-
-import com.onlyfy.recommendation_service.model.Recommendation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecommendationRepository extends JpaRepository<Recommendation, Integer> {
-}
