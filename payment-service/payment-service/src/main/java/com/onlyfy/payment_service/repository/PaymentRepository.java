69f0ed2da9e8b4dbaa0a73600ef6e2016896df08
gugardilcic-oss <gu.gardilcic@duocuc.cl>
2026-05-16 17:01:14 -0400

    Delete payment-service/payment-service directory

--- payment-service/payment-service/src/main/java/com/onlyfy/payment_service/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlyfy.payment_service.repository;
-
-import com.onlyfy.payment_service.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;   
-
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-}
