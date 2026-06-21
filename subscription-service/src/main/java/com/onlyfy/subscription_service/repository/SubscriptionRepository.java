13f62d0119472ba663d8a522276994afd3df1f21
Lukazez <lukazez17@gmail.com>
2026-06-21 00:47:01 -0400

    Delete subscription-service directory

--- subscription-service/src/main/java/com/onlyfy/subscription_service/repository/SubscriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.onlyfy.subscription_service.repository;
-
-import com.onlyfy.subscription_service.model.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
-}
